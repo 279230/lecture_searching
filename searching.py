d86e89a42c7cb0538b2c80cb11f10da225a68d49
Anika Unge <279230@vutbr.cz>
2026-04-13 13:04:32 +0200

    commit

--- searching.py
+++ searching.py
@@ -17,13 +17,27 @@ def read_data(file_name, field):
             - str: If field is 'dna_sequence'.
             - None: If the field is not supported.
     """
+    with open(file_name, "r") as file:
+        data = json.load(file)
+    keys = ["unordered_numbers", "ordered_numbers", "dna_sequence"]
+    for key in data:
+        if key != field:
+            return None
+        else:
+            vystup = data.get(field)
+            return vystup
+
     # get current working directory path
+
+
     cwd_path = Path.cwd()
-    
+
     file_path = cwd_path / file_name
 
 
 def main():
+    sequential_data = read_data("sequential.json", "unordered_numbers")
+    print(sequential_data)
     pass
 
 
